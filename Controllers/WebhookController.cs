4b4a9014e0065de2b37b4e93cab5a7baa99e520f
diegomalta <diego_malta@hotmail.com>
2021-10-04 22:28:20 -0500

    initial api

---
+++ Controllers/WebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webhook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebhookController : ControllerBase
+    {
+
+        public WebhookController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { text = "Hello" });
+        }
+        
+    }
+}
